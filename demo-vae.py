5c9f176923f890b9caa7a8fe63d95907e99f46e9
Proton <feisuzhu@163.com>
2026-01-30 16:42:47 +0800

    feat: A blog about overmind

--- demo-vae.py
+++ demo-vae.py
@@ -1,38 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# -- stdlib --
+# -- third party --
+from diffusers import ControlNetModel, StableDiffusionControlNetPipeline
+from diffusers.models import AutoencoderKL
 import overmind.api
+import pysnooper
+import torch
+
+# -- code --
 overmind.api.monkey_patch_all()
 
-import torch
-from diffusers.models import AutoencoderKL
+@pysnooper.snoop(relative_time=True, depth=2, color=False)
+def load_pipeline():
+    vae = (lambda: AutoencoderKL.from_pretrained(
+        "lemon2431/ChineseInkComicStrip_v10",
+        subfolder="vae",
+        torch_dtype=torch.float16,
+        device_map='cuda',
+    ))()
+    print(vae.device)
+    controlnet_depth = (lambda: ControlNetModel.from_pretrained(
+        "lllyasviel/control_v11f1p_sd15_depth",
+        torch_dtype=torch.float16,
+        variant="fp16",
+        device_map='cuda',
+    ))()
+    controlnet_edge = (lambda: ControlNetModel.from_pretrained(
+        "lllyasviel/control_v11p_sd15_softedge",
+        torch_dtype=torch.float16,
+        variant="fp16",
+        device_map='cuda',
+    ))()
 
-from diffusers import (
-    ControlNetModel,
-    StableDiffusionControlNetPipeline,
-)
+    pipeline = (lambda: StableDiffusionControlNetPipeline.from_pretrained(
+        "lemon2431/ChineseInkComicStrip_v10",
+        controlnet=[controlnet_edge, controlnet_depth],
+        vae=vae,
+        torch_dtype=torch.float16,
+        safety_checker=None,
+        device_map='cuda',
+    ))()
 
-checkpoint = "lemon2431/ChineseInkComicStrip_v10"
-vae = AutoencoderKL.from_pretrained(
-    "lemon2431/ChineseInkComicStrip_v10",
-    subfolder="vae",
-    torch_dtype=torch.float16,
-)
-controlnet_tile = ControlNetModel.from_pretrained(
-    "lllyasviel/control_v11f1e_sd15_tile", torch_dtype=torch.float16
-)
-controlnet_depth = ControlNetModel.from_pretrained(
-    "lllyasviel/control_v11f1p_sd15_depth",
-    torch_dtype=torch.float16,
-    variant="fp16",
-)
-controlnet_edge = ControlNetModel.from_pretrained(
-    "lllyasviel/control_v11p_sd15_softedge",
-    torch_dtype=torch.float16,
-    variant="fp16",
-)
+    # (lambda: pipeline.to('cuda'))()
+    print(pipeline.device)
 
-text2img_pipeline = StableDiffusionControlNetPipeline.from_pretrained(
-    checkpoint,
-    controlnet=[controlnet_edge, controlnet_depth],
-    vae=vae,
-    torch_dtype=torch.float16,
-    safety_checker=None,
-)
+load_pipeline()
